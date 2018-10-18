a617e43790b5ff28baa546f7848ed64305b1853c
Mathias Buus <mathiasbuus@gmail.com>
2018-10-18 20:07:33 +0200

    more inline tweaks

--- inline.js
+++ inline.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const wasm = fs.readFileSync(__dirname + '/binding.wasm', 'base64')
 const dataUrl = 'data:application/wasm;base64,' + wasm
 const src = fs.readFileSync(__dirname + '/binding.js', 'utf-8')
-  .replace(/var wasmBinaryFile = 'binding.wasm'/, `var wasmBinaryFile = ENVIRONMENT_IS_WEB ? '${dataUrl}' : 'binding.wasm'`)
+  .replace(/var wasmBinaryFile = 'binding.wasm'/, `var wasmBinaryFile = '${dataUrl}'`)
+  .replace(`require('fs')`, '{}')
 
 fs.writeFileSync(__dirname + '/binding-inline.js', src)
